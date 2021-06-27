f0e4691bd057927588d991cf4abf30044fd633df
Nathan Nguyen <nathannguyen@nathans-mbp-2.home>
2021-06-26 20:32:20 -0400

    db setup

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,27 @@ const demo = require('./demo.route');
 
 const r = Router();
 
-r.use('/demo', demo);
+// r.use('/demo', demo);
+r.use('/demo', (req, res) => {
+    const { MongoClient } = require('mongodb');
+    const uri = "mongodb+srv://whiterose:avengers21@cluster0.uimrt.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log(client);
+    client.connect(err => {
+        console.log("error: " + err);
+        console.log("Reading from collection");
+        // const collection = client.db("test").collection("resturants");
 
-r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+        var resturant = { name: "Obao"};
+        client.db("test").collection("resturants").insertOne(resturant, function(err, res) {
+            if (err) throw err;               
+            client.close();
+        });            
+    });
+});
+
+r.get('/', (req, res) => {
+    res.json(new SuccessResponseObject('express vercel boiler plate'))
+});
 
 module.exports = r;
