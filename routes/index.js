dc18037fcae345d175642d0963bbc133f7a6e258
Nathan Nguyen <nathannguyen@nathans-mbp-2.home>
2021-07-05 15:06:48 -0400

    add resturants routes

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,6 @@ r.use('/demo', (req, res) => {
     client.connect(err => {
         console.log("error: " + err);
         console.log("Reading from collection");
-        // const collection = client.db("test").collection("resturants");
-
         var resturant = { name: "Obao"};
         client.db("test").collection("resturants").insertOne(resturant, function(err, res) {
             if (err) throw err;               
@@ -23,8 +21,21 @@ r.use('/demo', (req, res) => {
     });
 });
 
+r.use('/resturants', (req, res) => {
+    const { MongoClient } = require('mongodb');
+    const uri = "mongodb+srv://whiterose:avengers21@cluster0.uimrt.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log(client);
+    client.connect(err => {
+        console.log("error: " + err);
+        console.log("Reading from collection");
+        console.log(client.db("test").collection("resturants").find().toArray().then(doc => res.json({doc})));        
+    });
+});
+
+
 r.get('/', (req, res) => {
-    res.json(new SuccessResponseObject('express vercel boiler plate'))
+    res.json(new SuccessResponseObject('express vercel boiler plate'));
 });
 
 module.exports = r;
