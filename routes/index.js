c5a7c76fe98ce3ba928654bbe589a0fcbb7fa763
Nathan Nguyen <micklebrain@gmail.com>
2022-05-01 16:01:08 -0400

    stripe

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var cors = require('cors')
 
 const r = Router();
 
+const stripe = require("stripe")('sk_test_51Kr8zwCrXyNi8bG6P9SuyD2PZ3LvelPqFjsJysI4rAnz8Y41cJdC5sN5HhdaefKvegsD7HbAfEKuldxT6rg1zgfd00XbGRmXxL');
+
 // Resolve cors
 r.use(cors())
 
@@ -15,11 +17,11 @@ r.get('/demo', (req, res) => {
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         console.log("error: " + err);
-        var resturant = { name: "Obao"};
-        client.db("test").collection("resturants").insertOne(resturant, function(err, res) {
-            if (err) throw err;               
+        var resturant = { name: "Obao" };
+        client.db("test").collection("resturants").insertOne(resturant, function (err, res) {
+            if (err) throw err;
             client.close();
-        });            
+        });
     });
 });
 
@@ -28,9 +30,9 @@ r.get('/resturants/:city', (req, res) => {
     const uri = "mongodb+srv://whiterose:avengers21@cluster0.uimrt.mongodb.net/test?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
-        console.log("error: " + err);       
+        console.log("error: " + err);
         var city = '' + req.params.city
-        client.db("test").collection("resturants").find({ city: city }).toArray().then(doc => res.json({doc}));           
+        client.db("test").collection("resturants").find({ city: city }).toArray().then(doc => res.json({ doc }));
     });
 });
 
@@ -41,7 +43,7 @@ r.get('/neighborhoodresturants/:city', (req, res) => {
     client.connect(err => {
         console.log("error: " + err);
         var city = '' + req.params.city
-        client.db("test").collection("topneighborhoodresturants").find({ city: city }).toArray().then(doc => res.json({doc}));           
+        client.db("test").collection("topneighborhoodresturants").find({ city: city }).toArray().then(doc => res.json({ doc }));
     });
 });
 
@@ -52,13 +54,37 @@ r.get('/events/:city', (req, res) => {
     client.connect(err => {
         console.log("error: " + err);
         var city = '' + req.params.city
-        client.db("test").collection("events").find({ city: city }).toArray().then(doc => res.json({doc}));           
+        client.db("test").collection("events").find({ city: city }).toArray().then(doc => res.json({ doc }));
     });
 });
 
 
 r.get('/', (req, res) => {
     res.json(new SuccessResponseObject('express vercel boiler plate'));
+});
+
+const calculateOrderAmount = (items) => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400;
+};
+
+r.post('/create-payment-intent', async (req, res) => {
+    const { items } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd",
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
 });
 
 module.exports = r;
