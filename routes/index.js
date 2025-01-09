b07e2a28058c30cce5676b3fc4c30d4193b32d25
Nathan Nguyen <micklebrain@gmail.com>
2025-01-08 22:25:28 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -18,17 +18,41 @@ r.get('/', (req, res) => {
 
 // r.use('/demo', demo);
 r.get('/demo', (req, res) => {
-    const { MongoClient } = require('mongodb');
+
+    // const { MongoClient } = require('mongodb');
+    // const uri = "mongodb+srv://whiterose:avengers21@micklebrain.uimrt.mongodb.net/?retryWrites=true&w=majority&appName=micklebrain";
+    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // client.connect(err => {
+    //     console.log("error: " + err);
+    //     var resturant = { name: "Obao" };
+    //     client.db("test").collection("resturants").insertOne(resturant, function (err, res) {
+    //         if (err) throw err;
+    //         client.close();
+    //     });
+    // });
+
+    const { MongoClient, ServerApiVersion } = require('mongodb');
     const uri = "mongodb+srv://whiterose:avengers21@micklebrain.uimrt.mongodb.net/?retryWrites=true&w=majority&appName=micklebrain";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        console.log("error: " + err);
-        var resturant = { name: "Obao" };
-        client.db("test").collection("resturants").insertOne(resturant, function (err, res) {
-            if (err) throw err;
-            client.close();
-        });
-    });
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+      });
+
+      try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+        // Send a ping to confirm a successful connection
+        client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      } finally {
+        // Ensures that the client will close when you finish/error
+        client.close();
+      }
+
 });
 
 r.get('/resturants/:city', (req, res) => {
