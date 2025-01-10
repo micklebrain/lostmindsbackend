441b7b3a6d679d5fd2cbd3d504829192a0422112
Nathan Nguyen <micklebrain@gmail.com>
2025-01-09 16:20:58 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -19,16 +19,28 @@ r.get('/', (req, res) => {
 // r.use('/demo', demo);
 r.get('/demo', (req, res) => {
 
-    const { MongoClient, ServerApiVersion } = require('mongodb');
-    const uri = "mongodb+srv://whiterose:avengers21@micklebrain.uimrt.mongodb.net/?retryWrites=true&w=majority&appName=micklebrain";
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
+    // const { MongoClient, ServerApiVersion } = require('mongodb');
+    // const uri = "mongodb+srv://whiterose:avengers21@micklebrain.uimrt.mongodb.net/?retryWrites=true&w=majority&appName=micklebrain";
+    // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    // const client = new MongoClient(uri, {
+    //     serverApi: {
+    //         version: ServerApiVersion.v1,
+    //         strict: true,
+    //         deprecationErrors: true,
+    //     }
+    // });
+
+    mongoose.connect('mongodb+srv://whiterose:avengers21@cluster0.uimrt.mongodb.net/test?retryWrites=true&w=majority');
+    const database = mongoose.connection
+
+    database.on('error', (error) => {
+        console.log(error)
+    })
+    
+    database.once('connected', () => {
+        console.log('Database Connected');
+    })
+
     // try {
     //     // Connect the client to the server	(optional starting in v4.7)
     //     client.connect();
