87470ceacdb2263aa39d790e2285d68a381644de
Nathan Nguyen <micklebrain@gmail.com>
2025-01-09 18:51:53 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ r.get('/demo', (req, res) => {
     // })
 
     const { MongoClient, ServerApiVersion } = require('mongodb');
-    const uri = "mongodb+srv://whiterose:avengers21@micklebrain.uimrt.mongodb.net/?retryWrites=true&w=majority&appName=micklebrain";
+    const uri = "mongodb+srv://whiterose:avengers21@micklebrain.uimrt.mongodb.net/?retryWrites=true&w=majority";
     // Create a MongoClient with a MongoClientOptions object to set the Stable API version
     const client = new MongoClient(uri, {
         serverApi: {
@@ -47,8 +47,8 @@ r.get('/demo', (req, res) => {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
         // Send a ping to confirm a successful connection
-        client.db("todo").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // client.db("todo").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         client.close();
